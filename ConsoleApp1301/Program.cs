b912b2fc233d0ee700c807b9a142d667f333ff17
zilan.gunduz <zilan.gunduz@101-14>
2023-01-13 19:53:58 +0300

    Add project files.

---
+++ ConsoleApp1301/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp1301
+{
+    internal class Program
+    {
+        //TODO: Daha çook iş var
+        //Fix: Ne
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.func1();
+        }
+
+        public void func1()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(i);
+                System.Threading.Thread.Sleep(500);
+                if (i == 8)
+                {
+                    System.Diagnostics.Debugger.Launch();
+                    //System.Diagnostics.Debugger.Break();
+                }
+
+                //System.Diagnostics.Debugger.Launch();
+            }
+        }
+    }
+}
